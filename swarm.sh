a40a9b01700aa079447d9ea9789676fbc4a40a38
Daniel Mermelstein <danmermel@gmail.com>
2016-12-30 19:16:21 +0000

    swarm.sh

---
+++ swarm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+swarm --recursive up plot_1/
+swarm --recursive up plot_2/
+swarm --recursive up plot_3/
+swarm --recursive up plot_4/
+swarm --recursive up plot_5/
+swarm --recursive up sadc/
+swarm --recursive up sa_estate/
+swarm --recursive up sa_estate_minus_sadc/
+swarm --recursive up santa_ana_minus_1/
+swarm --recursive up santa_ana_minus_2/
+swarm --recursive up santa_ana_minus_3/
+swarm --recursive up santa_ana_minus_4/
+swarm --recursive up santa_ana_minus_5/
